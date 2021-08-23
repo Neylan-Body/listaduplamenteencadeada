094a20ff72a099040bc747ba05ba1ad1b3494819
Neylan <neylan2011@gmail.com>
2021-08-23 11:06:32 -0300

    Final commit

--- src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -50,6 +50,32 @@ public class ListaDuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    public void remove(int index){
+        if (index == 0){
+            primeiroNo = primeiroNo.getNoProximo();
+            if (primeiroNo!= null){
+                primeiroNo.setNoPrevio(null);
+            }
+        }else{
+            NoDuplo<T> noAuxiliar = getNo(index);
+            noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+            if (noAuxiliar != ultimoNo){
+                noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());
+            }else{
+                ultimoNo = noAuxiliar.getNoPrevio();
+            }
+        }
+        tamanhoLista--;
+    }
+
+    public NoDuplo<T> last(){
+        return ultimoNo;
+    }
+
+    public NoDuplo<T> first(){
+        return primeiroNo;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
         for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
@@ -60,5 +86,18 @@ public class ListaDuplamenteEncadeada<T> {
 
     private int size(){
         return this.tamanhoLista;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; i < this.size(); i++) {
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += "null";
+        return strRetorno;
     }
 }
---
+++ src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> listaDuplamenteEncadeada = new ListaDuplamenteEncadeada<>();
+        listaDuplamenteEncadeada.add("c1");
+        listaDuplamenteEncadeada.add("c2");
+        listaDuplamenteEncadeada.add("c3");
+        listaDuplamenteEncadeada.add("c4");
+        listaDuplamenteEncadeada.add("c5");
+        listaDuplamenteEncadeada.add("c6");
+        listaDuplamenteEncadeada.add("c7");
+        System.out.println(listaDuplamenteEncadeada);
+        listaDuplamenteEncadeada.remove(6);
+        System.out.println(listaDuplamenteEncadeada);
+        System.out.println(listaDuplamenteEncadeada.last());
+        System.out.println(listaDuplamenteEncadeada.first());
+    }
+}
